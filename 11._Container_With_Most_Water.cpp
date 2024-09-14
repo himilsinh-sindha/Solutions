5d2a693e71a43955e0b719ee48ade0f7acabdce3
Himilsinh Sindha <102691151+himilsinh-sindha@users.noreply.github.com>
2024-09-14 20:29:22 +0530

    Solved 11._Container_With_Most_Water.cpp

--- 11._Container_With_Most_Water.cpp
+++ 11._Container_With_Most_Water.cpp
@@ -1,21 +1,69 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int maxArea = 0;
+    int maxArea(vector<int> &arr)
+{
+    vector<int> max(2);
+    vector<int> temp1(2);
+    vector<int> temp2(2);
+    int j;
+    max[1] = 1;
+    if(arr[0]<arr[1]) {
+    max[0] = arr[0];
+    }
+    else{
+        max[0] = arr[1];
+    }
+    for (int i = 0; i < arr.size() - 2; i++)
+    {
+        for (int j = i + 1; j < arr.size()-1; j++)
+        {
+            temp1[1] = j - i;
+            if (arr[i] > arr[j])
+            {
+                temp1[0] = arr[j]; 
+                
+            }
+            else
+            {
+                temp1[0] = arr[i];
+                
+            }
 
-        while (left < right) {
-            int currentArea = min(height[left], height[right]) * (right - left);
-            maxArea = max(maxArea, currentArea);
+                temp2[1] = j + 1 - i;
+            if (arr[i] > arr[j + 1])
+            {
+                temp2[0] = arr[j+1];
+               
+            }
+            else
+            {
+                temp2[0] = arr[i];
+                
+             }
 
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
+            if (temp1[0] * temp1[1] > temp2[0] * temp2[1])
+            {
+                if (max[0] * max[1] < temp1[0] * temp1[1])
+                {
+
+                    max[0] = temp1[0];
+                    max[1] = temp1[1];
+                }
+                
+            }
+            else
+            {
+
+                if (max[0] * max[1] < temp2[0] * temp2[1])
+                {
+                    max[0] = temp2[0];
+                    max[1] = temp2[1];
+                }
+                
             }
         }
-
-        return maxArea;
     }
+
+    return max[0] * max[1];
+}
 };
