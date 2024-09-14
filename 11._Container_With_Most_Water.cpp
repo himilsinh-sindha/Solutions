b911af1b6d8bb24fdb14a3cceffff5632192799d
Himilsinh Sindha <102691151+himilsinh-sindha@users.noreply.github.com>
2024-09-14 21:40:21 +0530

    Solved 11._Container_With_Most_Water.cpp

--- 11._Container_With_Most_Water.cpp
+++ 11._Container_With_Most_Water.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-   int maxArea(vector<int> &arr)
-{
-    vector<int> maximum(2);
-    vector<int> temp1(2);
-    vector<int> temp2(2);
-    int j;
-    int max1;
-    int max2;
-    int maxValue;
-    maximum[1] = 1;
-    maximum[0] = min(arr[0], arr[1]);
-    maxValue = maximum[0] * maximum[1];
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
 
-    for (int i = 0; i < arr.size() - 2; i++)
-    {
-        for (int j = i + 1; j < arr.size() - 1; j++)
-        {
-            temp1[1] = j - i;
-            temp1[0] = min(arr[i], arr[j]);
+        while (left < right) {
+            int currentArea = min(height[left], height[right]) * (right - left);
+            maxArea = max(maxArea, currentArea);
 
-            temp2[1] = j + 1 - i;
-            temp2[0] = min(arr[j + 1], arr[i]);
-
-
-            max1 = temp1[0] * temp1[1];
-            max2 = temp2[0] * temp2[1];
-
-            if (maxValue < max(max1, max2))
-            {
-
-                maxValue = max(max1, max2);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
-    }
 
-    return maxValue;
-}
+        return maxArea;
+    }
 };
