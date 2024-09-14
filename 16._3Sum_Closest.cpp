bb941c55f9356b7251340f914b2f1adc384b6ce6
Himilsinh Sindha <102691151+himilsinh-sindha@users.noreply.github.com>
2024-09-14 20:15:23 +0530

    Solved 16._3Sum_Closest.cpp

--- 16._3Sum_Closest.cpp
+++ 16._3Sum_Closest.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        int closestNumber = nums[0]+nums[1]+nums[2];
+        int closestSum = nums[0] + nums[1] + nums[2]; // Initialize with the sum of the first three elements
         
-
-        for (int i = 0; i < nums.size() - 2; i++) {
-           int left = i + 1;
-            int right = nums.size() - 1;
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            
             while (left < right) {
-                int currentNumber = nums[i] + nums[left] + nums[right];
-
-                if(abs(currentNumber-target) < abs(closestNumber-target)){
-                    closestNumber = currentNumber;
+                int currentSum = nums[i] + nums[left] + nums[right];
+                
+                if (abs(currentSum - target) < abs(closestSum - target)) {
+                    closestSum = currentSum; // Update the closestSum if the currentSum is closer to the target
                 }
-
-                if (currentNumber < target) {
+                
+                if (currentSum < target) {
                     left++;
                 } else {
                     right--;
                 }
             }
         }
-
-        return closestNumber;
+        
+        return closestSum;
     }
 };
