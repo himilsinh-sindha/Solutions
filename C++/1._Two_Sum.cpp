0f3cef8c57cdcbda232562618e361ccf2bdd3f16
Himilsinh Sindha <102691151+himilsinh-sindha@users.noreply.github.com>
2024-10-10 01:36:13 +0530

    Solved 1._Two_Sum.cpp

--- C++/1._Two_Sum.cpp
+++ C++/1._Two_Sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i=0;i<=size(nums)-1;i++){
-            for(int j=i+1;j<=size(nums)-1;j++)
-            
-            if(nums[i]+nums[j]==target){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
+        map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            int a = nums[i];
+            int req= target - a;
+            if(m.find(req)!=m.end()){
+                return {m[req],i};
             }
+            m[a]=i;
         }
-        return ans;
+
+        return nums;
     }
 };
